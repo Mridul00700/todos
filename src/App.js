a3339c0c7381c6772740013946897b2c61cd5ba5
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-27 10:14:28 +0530

    style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import Todo from './components/Todo';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+
+    </div>
   );
 }
 
